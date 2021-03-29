7de28566ed0a875b14c6871882e0ae0678361546
kgw0422 <kgw660386@gmail.com>
2021-03-29 12:37:21 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-  end
-
-  def new
+    @posts = Post.order(id: "DESC")
   end
 
   def create
     Post.create(content: params[:content])
+    redierct_to action: :index 
   end
 end
